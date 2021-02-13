b6401aae21bfbeacdc023e13742981318c0bd2fc
Jhan Carlos Carvajal Bastidas <liceo@DESKTOP-LNJO0VF>
2021-02-12 21:39:23 -0500

    I added the FXML login and register for now, also the login screen and an event to display the register screen

--- src/ui/ClassroomGUI.java
+++ src/ui/ClassroomGUI.java
@@ -1,5 +1,35 @@
 package ui;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
 public class ClassroomGUI {
+	
+    @FXML
+    private BorderPane mainPanel;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    void singUp(ActionEvent event) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("register.fxml"));
+		
+		fxmlLoader.setController(this);    	
+		Parent addContactPane = fxmlLoader.load();
+    	
+		mainPanel.getChildren().clear();
+    	mainPanel.setTop(addContactPane);
+    }
 
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,24 @@
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main extends Application{
 
+	public static void main (String[] args) {
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+		
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Classroom");
+		primaryStage.show();
+	}
 }
